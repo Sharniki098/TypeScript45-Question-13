fda8286c48a15814d02951018505ec422cecbb57
Shumaila Noreen <161513956+Sharniki098@users.noreply.github.com>
2024-05-20 01:30:06 -0700

    Add files via upload

---
+++ YourOwnArray.js
@@ -0,0 +1,4 @@
+var transportation = ["Motercycle", "Car", "Train", "Cycle"];
+for (var i = 0; i < transportation.length; i++) {
+    console.log("I Would like to own a  " + transportation[i]);
+}
---
+++ YourOwnArray.ts
@@ -0,0 +1,8 @@
+
+
+let transportation:string[]=["Motercycle","Car","Train", "Cycle" ] ;
+
+for(let i=0; i<transportation.length; i++){
+ console.log("I Would like to own a  " +transportation[i])  
+
+}
